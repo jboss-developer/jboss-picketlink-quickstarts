c381f17aebdc95fac0f1b5462f2aa4f3b4f4ec0c
Pedro Igor <pigor.craveiro@gmail.com>
2014-03-26 12:26:55 -0300

    Using the PartitionManagerCreateEvent to initialize partitions instead.

--- picketlink-authentication-idm-multi-tenancy/src/main/java/org/jboss/as/quickstarts/picketlink/idm/multitenancy/jsf/IDMInitializer.java
+++ picketlink-authentication-idm-multi-tenancy/src/main/java/org/jboss/as/quickstarts/picketlink/idm/multitenancy/jsf/IDMInitializer.java
@@ -16,43 +16,39 @@
  */
 package org.jboss.as.quickstarts.picketlink.idm.multitenancy.jsf;
 
+import org.picketlink.PartitionManagerCreateEvent;
 import org.picketlink.idm.IdentityManager;
 import org.picketlink.idm.PartitionManager;
 import org.picketlink.idm.credential.Password;
 import org.picketlink.idm.model.basic.Realm;
 import org.picketlink.idm.model.basic.User;
 
-import javax.annotation.PostConstruct;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.inject.Inject;
+import javax.ejb.Stateless;
+import javax.enterprise.event.Observes;
 
-@Startup
-@Singleton
+@Stateless
 public class IDMInitializer {
-
-    @Inject
-    private PartitionManager partitionManager;
 
     /**
      * <p>Creates some default users for each realm/company.</p>
      */
-    @PostConstruct
-    public void createDefaultUsers() {
-        createUserForRealm(Resources.REALM.acme.name(), "bugs");
-        createUserForRealm(Resources.REALM.umbrella.name(), "jill");
-        createUserForRealm(Resources.REALM.wayne.name(), "bruce");
+    public void createDefaultUsers(@Observes PartitionManagerCreateEvent event) {
+        PartitionManager partitionManager = event.getPartitionManager();
+
+        createUserForRealm(partitionManager, Resources.REALM.acme.name(), "bugs");
+        createUserForRealm(partitionManager, Resources.REALM.umbrella.name(), "jill");
+        createUserForRealm(partitionManager, Resources.REALM.wayne.name(), "bruce");
     }
 
-    private void createUserForRealm(String realmName, String loginName) {
-        Realm partition = this.partitionManager.getPartition(Realm.class, realmName);
+    private void createUserForRealm(PartitionManager partitionManager, String realmName, String loginName) {
+        Realm partition = partitionManager.getPartition(Realm.class, realmName);
 
         if (partition == null) {
             partition = new Realm(realmName);
-            this.partitionManager.add(partition);
+            partitionManager.add(partition);
         }
 
-        IdentityManager identityManager = this.partitionManager.createIdentityManager(partition);
+        IdentityManager identityManager = partitionManager.createIdentityManager(partition);
 
         User user = new User(loginName);
         Password password = new Password(user.getLoginName() + "123");
@@ -60,5 +56,4 @@ public class IDMInitializer {
         identityManager.add(user);
         identityManager.updateCredential(user, password);
     }
-
 }
