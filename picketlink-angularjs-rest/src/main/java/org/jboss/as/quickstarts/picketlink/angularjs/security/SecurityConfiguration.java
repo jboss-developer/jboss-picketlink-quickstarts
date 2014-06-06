fc019e719b5f259b8f6de3ac8c69c0dae73f114c
Pedro Igor <pigor.craveiro@gmail.com>
2014-06-06 13:54:50 -0300

    More documentation.

--- picketlink-angularjs-rest/src/main/java/org/jboss/as/quickstarts/picketlink/angularjs/security/SecurityConfiguration.java
+++ picketlink-angularjs-rest/src/main/java/org/jboss/as/quickstarts/picketlink/angularjs/security/SecurityConfiguration.java
@@ -21,18 +21,13 @@
  */
 package org.jboss.as.quickstarts.picketlink.angularjs.security;
 
-import org.jboss.as.quickstarts.picketlink.angularjs.model.Person;
 import org.jboss.as.quickstarts.picketlink.angularjs.security.authentication.JWSTokenProvider;
-import org.jboss.as.quickstarts.picketlink.angularjs.security.model.ApplicationRole;
 import org.jboss.as.quickstarts.picketlink.angularjs.security.model.MyUser;
 import org.jboss.as.quickstarts.picketlink.angularjs.security.model.entity.MyUserTypeEntity;
-import org.picketlink.IdentityConfigurationEvent;
-import org.picketlink.PartitionManagerCreateEvent;
-import org.picketlink.idm.IdentityManager;
-import org.picketlink.idm.PartitionManager;
-import org.picketlink.idm.config.IdentityConfigurationBuilder;
-import org.picketlink.idm.config.SecurityConfigurationException;
-import org.picketlink.idm.credential.Password;
+import org.picketlink.annotations.PicketLink;
+import org.picketlink.authentication.web.TokenAuthenticationScheme;
+import org.picketlink.config.SecurityConfigurationBuilder;
+import org.picketlink.event.SecurityConfigurationEvent;
 import org.picketlink.idm.credential.handler.TokenCredentialHandler;
 import org.picketlink.idm.jpa.model.sample.simple.AttributeTypeEntity;
 import org.picketlink.idm.jpa.model.sample.simple.GroupTypeEntity;
@@ -43,32 +38,20 @@ import org.picketlink.idm.jpa.model.sample.simple.RelationshipIdentityTypeEntity
 import org.picketlink.idm.jpa.model.sample.simple.RelationshipTypeEntity;
 import org.picketlink.idm.jpa.model.sample.simple.RoleTypeEntity;
 import org.picketlink.idm.jpa.model.sample.simple.TokenCredentialTypeEntity;
-import org.picketlink.idm.model.Attribute;
-import org.picketlink.idm.model.basic.Realm;
-import org.picketlink.idm.model.basic.Role;
 import org.picketlink.internal.EEJPAContextInitializer;
 
-import javax.ejb.Stateless;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-
-import static org.jboss.as.quickstarts.picketlink.angularjs.security.model.IdentityModelUtils.findByLoginName;
-import static org.picketlink.idm.model.basic.BasicModel.getRole;
-import static org.picketlink.idm.model.basic.BasicModel.grantRole;
 
 /**
+ * <p>This class is responsible to enable the {@link org.picketlink.authentication.web.TokenAuthenticationScheme}.</p>
+ *
  * @author Pedro Igor
  */
-@Stateless
+@ApplicationScoped
 public class SecurityConfiguration {
-
-    public static final String KEYSTORE_FILE_PATH = "/keystore.jks";
-
-    private KeyStore keyStore;
 
     @Inject
     private JWSTokenProvider tokenProvider;
@@ -76,120 +59,39 @@ public class SecurityConfiguration {
     @Inject
     private EEJPAContextInitializer contextInitializer;
 
-    public void configureIdentityManagement(@Observes IdentityConfigurationEvent event) {
-        IdentityConfigurationBuilder builder = event.getConfig();
+    @Inject
+    private TokenAuthenticationScheme tokenAuthenticationScheme;
+
+    @Produces
+    @PicketLink
+    public TokenAuthenticationScheme configureTokenAuthenticationScheme() {
+        return this.tokenAuthenticationScheme;
+    }
+
+    public void configureIdentityManagement(@Observes SecurityConfigurationEvent event) {
+        SecurityConfigurationBuilder builder = event.getBuilder();
 
         builder
+            .identity()
+            .stateless()
+            .idmConfig()
             .named("default.config")
-                .stores()
-                    .jpa()
-                        .mappedEntity(
-                            PartitionTypeEntity.class,
-                            RoleTypeEntity.class,
-                            GroupTypeEntity.class,
-                            IdentityTypeEntity.class,
-                            RelationshipTypeEntity.class,
-                            RelationshipIdentityTypeEntity.class,
-                            PasswordCredentialTypeEntity.class,
-                            TokenCredentialTypeEntity.class,
-                            AttributeTypeEntity.class,
-                            MyUserTypeEntity.class)
-                        .addContextInitializer(this.contextInitializer)
-                        .setCredentialHandlerProperty(TokenCredentialHandler.TOKEN_PROVIDER, this.tokenProvider)
-                        .supportType(MyUser.class)
-                        .supportAllFeatures();
-    }
-
-    public void configureDefaultPartition(@Observes PartitionManagerCreateEvent event) {
-        PartitionManager partitionManager = event.getPartitionManager();
-
-        createDefaultPartition(partitionManager);
-        createDefaultRoles(partitionManager);
-        createAdminAccount(partitionManager);
-    }
-
-    private void createDefaultRoles(PartitionManager partitionManager) {
-        IdentityManager identityManager = partitionManager.createIdentityManager();
-
-        createRole(ApplicationRole.ADMINISTRATOR, identityManager);
-        createRole(ApplicationRole.USER, identityManager);
-    }
-
-    private void createDefaultPartition(PartitionManager partitionManager) {
-        Realm partition = partitionManager.getPartition(Realm.class, Realm.DEFAULT_REALM);
-
-        if (partition == null) {
-            try {
-                partition = new Realm(Realm.DEFAULT_REALM);
-
-                partition.setAttribute(new Attribute<byte[]>("PublicKey", getPublicKey()));
-                partition.setAttribute(new Attribute<byte[]>("PrivateKey", getPrivateKey()));
-
-                partitionManager.add(partition);
-            } catch (Exception e) {
-                throw new SecurityConfigurationException("Could not create default partition.", e);
-            }
-        }
-    }
-
-    public static Role createRole(ApplicationRole applicationRole, IdentityManager identityManager) {
-        String roleName = applicationRole.name();
-        Role role = getRole(identityManager, roleName);
-
-        if (role == null) {
-            role = new Role(roleName);
-            identityManager.add(role);
-        }
-
-        return role;
-    }
-
-    private byte[] getPrivateKey() throws KeyStoreException, NoSuchAlgorithmException, UnrecoverableKeyException {
-        return getKeyStore().getKey("servercert", "test123".toCharArray()).getEncoded();
-    }
-
-    private byte[] getPublicKey() throws KeyStoreException {
-        return getKeyStore().getCertificate("servercert").getPublicKey().getEncoded();
-    }
-
-    private KeyStore getKeyStore() {
-        if (this.keyStore == null) {
-            try {
-                this.keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-                getKeyStore().load(getClass().getResourceAsStream(KEYSTORE_FILE_PATH), "store123".toCharArray());
-            } catch (Exception e) {
-                throw new SecurityException("Could not load key store.", e);
-            }
-        }
-
-        return this.keyStore;
-    }
-
-    public void createAdminAccount(PartitionManager partitionManager) {
-        IdentityManager identityManager = partitionManager.createIdentityManager();
-        String email = "admin@picketlink.org";
-
-        // if admin exists dont create again
-        if(findByLoginName(email, identityManager) != null) {
-            return;
-        }
-
-        Person person = new Person();
-
-        person.setFirstName("Almight");
-        person.setLastName("Administrator");
-        person.setEmail(email);
-
-        MyUser admin = new MyUser(person.getEmail());
-
-        admin.setPerson(person);
-
-        identityManager.add(admin);
-
-        identityManager.updateCredential(admin, new Password("admin"));
-
-        Role adminRole = getRole(identityManager, ApplicationRole.ADMINISTRATOR.name());
-
-        grantRole(partitionManager.createRelationshipManager(), admin, adminRole);
+            .stores()
+            .jpa()
+            .mappedEntity(
+                PartitionTypeEntity.class,
+                RoleTypeEntity.class,
+                GroupTypeEntity.class,
+                IdentityTypeEntity.class,
+                RelationshipTypeEntity.class,
+                RelationshipIdentityTypeEntity.class,
+                PasswordCredentialTypeEntity.class,
+                TokenCredentialTypeEntity.class,
+                AttributeTypeEntity.class,
+                MyUserTypeEntity.class)
+            .addContextInitializer(this.contextInitializer)
+            .setCredentialHandlerProperty(TokenCredentialHandler.TOKEN_PROVIDER, this.tokenProvider)
+            .supportType(MyUser.class)
+            .supportAllFeatures();
     }
 }
